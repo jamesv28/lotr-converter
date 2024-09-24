6f848be6aa807dcaffb99207d17dbe462a44afdf
James Volmert <james.volmert@gmail.com>
2024-09-24 17:19:51 -0600

    learned how to z stack images to overlay text

--- lotr-converter/SelectCurrency.swift
+++ lotr-converter/SelectCurrency.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // background image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                Text("Select the currency you are starting with: ")
+                    .fontWeight(.bold)
+                    
+                CurrencyIcon(currencyImage: .copperpenny , currencyName: "Copper Penny")
+                
+                Text("Selcect the currency you want to convert with")
+                    .fontWeight(.bold)
+                
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
     }
 }
 
